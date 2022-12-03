326601d4ede5b1ab89a7c3dd610f03c0fafa5bce
Daniel Diaz <710147@student.dusd.net>
2022-12-03 12:45:20 -0800

    added scripts to ruby and health collectables.
    added scripts to ruby and health collectables.

--- RubysAdventureDiaz/Assets/Scripts/RubyController.cs
+++ RubysAdventureDiaz/Assets/Scripts/RubyController.cs
@@ -4,29 +4,47 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+
+    Rigidbody2D rigidbody2D;
+    float horizontal;
+    float vertical;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Creating the two variables that use the pre built axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        //Create the movement vector
-        Vector2 position = transform.position;
-
-        //changing the x an y position
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        //Set the update position
-        transform.position = position;
-        
-
+        rigidbody2D.MovePosition(position);
     }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
