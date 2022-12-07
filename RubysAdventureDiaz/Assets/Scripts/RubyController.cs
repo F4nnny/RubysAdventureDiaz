eca39bc54bde593a3a0107b7dd17cad011ed39ea
Daniel Diaz <710147@student.dusd.net>
2022-12-06 23:11:46 -0800

    added 2 more scripts.
    added 2 more scripts.

--- RubysAdventureDiaz/Assets/Scripts/RubyController.cs
+++ RubysAdventureDiaz/Assets/Scripts/RubyController.cs
@@ -7,21 +7,24 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
 
     public int health { get { return currentHealth; }}
     int currentHealth;
 
+    bool isInvincible;
+    float InvincibleTimer;
+
     Rigidbody2D rigidbody2D;
     float horizontal;
     float vertical;
-
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
+        currentHealth = 4;
     }
 
     // Update is called once per frame
@@ -30,6 +33,13 @@ public class RubyController : MonoBehaviour
         //Creating the two variables that use the pre built axes
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            timeInvincible -= timeInvincible;
+            if (InvincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
@@ -43,6 +53,13 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            isInvincible = true;
+            InvincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
