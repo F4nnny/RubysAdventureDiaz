69475d2811024b3d19a4179bfbcf9e293317a4cb
Daniel Diaz <710147@student.dusd.net>
2022-12-09 11:47:50 -0800

    added animation to ruby and robot.
    added animation to ruby and robot.

--- RubysAdventureDiaz/Assets/Scripts/RubyController.cs
+++ RubysAdventureDiaz/Assets/Scripts/RubyController.cs
@@ -19,10 +19,14 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
         currentHealth = 4;
     }
@@ -33,6 +37,18 @@ public class RubyController : MonoBehaviour
         //Creating the two variables that use the pre built axes
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -55,8 +71,10 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
+
             isInvincible = true;
             InvincibleTimer = timeInvincible;
         }
